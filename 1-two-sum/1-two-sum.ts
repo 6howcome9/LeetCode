f48a5cb56d80da01b6663cd797d922d8ada69b80
Sunyoung <6howcome9@gmail.com>
2022-09-27 22:59:59 +0900

    Time: 456 ms (6.15%), Space: 45.1 MB (47.92%) - LeetHub

--- 1-two-sum/1-two-sum.ts
+++ 1-two-sum/1-two-sum.ts
@@ -3,8 +3,7 @@ function twoSum(nums: number[], target: number): number[] {
     let result: [number, number] = [0, 0];
     
     for (let i = 0; i < nums.length - 1; i++) {
-        const num: number = target - nums[i];
-        const idx: number = nums.lastIndexOf(num);
+        const idx: number = nums.lastIndexOf(target - nums[i]);
         
         if (i !== idx && idx > 0) {
             result = [i, idx];
