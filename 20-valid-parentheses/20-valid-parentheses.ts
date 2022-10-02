be45ff323fe8610d5564a9e907e95bb91821fb9f
Sunyoung <6howcome9@gmail.com>
2022-10-02 21:20:25 +0900

    Time: 95 ms (74.23%), Space: 45.9 MB (12.02%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.ts
+++ 20-valid-parentheses/20-valid-parentheses.ts
@@ -1,21 +1,20 @@
 function isValid(s: string): boolean {
     const brackets: {[key: string]: string} = {
-        "[": "]",
         "{": "}",
+        "[": "]",
         "(": ")"
     }
     
     let stack: string[] = [];
     
     for(let i = 0; i < s.length; i++) {
-        if (Object.keys(brackets).includes(s[i])) {
-            stack.push(s[i]);
+        if (["(", "[", "{"].includes(s[i])) {
+            stack.push(brackets[s[i]])
         } else {
-            if (brackets[stack.pop()] !== s[i]) {
-                return false;
+            if(stack.pop() !== s[i]) {
+                return false
             }
         }
     }
-    
     return stack.length === 0;
 };
