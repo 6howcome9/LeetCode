06cac03b276683f25cd677ac44265f3ebb41fc5f
Sunyoung <6howcome9@gmail.com>
2022-10-16 19:42:25 +0900

    Time: 115 ms (26.78%), Space: 42.7 MB (84.56%) - LeetHub

--- 0066-plus-one/0066-plus-one.ts
+++ 0066-plus-one/0066-plus-one.ts
@@ -1,17 +1,15 @@
 function plusOne(digits: number[]): number[] {
-    let result: number[] = [...digits];
-    
     for(let i = digits.length - 1; i >= 0; i--) {
         if (digits[i] + 1 === 10) {
-            result[i] = 0;
-            if(i === 0) {
-                result.unshift(1);
+            digits[i] = 0;
+            if (i === 0) {
+                digits.unshift(1);
             }
         } else {
-            result[i] = result[i] + 1;
+            digits[i] = digits[i] + 1;
             break;
         }
     }
     
-    return result;
+    return digits;
 };
